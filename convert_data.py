20d8d5ab212ea5ec227c870b6e0ace5b09075bd1
karthik-hegde <hegdekartik7@gmail.com>
2017-09-19 18:35:29 -0500

    Added k-mean based weight sharing

--- convert_data.py
+++ convert_data.py
@@ -6,6 +6,9 @@ data = np.load("bvlc_alexnet.npy").item()
 #data_8bit = convert_npy_8b(data)
 #np.save("data_8bit.npy", data_8bit)
 
-data_16bit = convert_npy_16b(data)
-np.save("data_16bit.npy", data_16bit)
+#data_16bit = convert_npy_16b(data)
+#np.save("data_16bit.npy", data_16bit)
+
+data_kmeans = convert_npy_kmeans(data)
+np.save("data_kmeans.npy", data_kmeans)
 
---
+++ math_helpers.py
@@ -0,0 +1,12 @@
+import numpy as np
+from scipy.cluster.vq import vq, kmeans
+
+
+#Perform kmeans classification on a given array and return codebook and indices
+
+def kmeans_data(x,centroids):
+	kmeans_x = kmeans(x, centroids)
+	vq_x	 = vq(kmeans_x[0])
+
+	return kmeans_x[0],vq_x[0]
+
