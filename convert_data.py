b0429a64f6f25497f3ee6ba81906b7614c53835b
rohitagr7 <rohitagr7@users.noreply.github.com>
2017-09-20 15:52:32 -0500

    Adding normal distribution based weight sharing

--- convert_data.py
+++ convert_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 from npy_process import *
-
+NUM_WEIGHTS_CONV = 256
+NUM_WEIGHTS_FC = 128
 data = np.load("bvlc_alexnet.npy").item()
 
 #data_8bit = convert_npy_8b(data)
@@ -10,5 +11,7 @@ data = np.load("bvlc_alexnet.npy").item()
 #np.save("data_16bit.npy", data_16bit)
 
 data_kmeans = convert_npy_kmeans(data)
+data_normal = convert_npy_normal(data, NUM_WEIGHTS_CONV, NUM_WEIGHTS_FC)
 np.save("data_kmeans.npy", data_kmeans)
+np.save("data_normal.npy", data_normal)
 
